441404924812b9373f25c165739bc6d9c7c93c37
Len Hulley <lenhulley@gmail.com>
2026-07-10 18:24:50 +0200

    Add org number, founding year and postcode from company registration

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,7 +1,9 @@
 module.exports = {
   url: "https://elephas-consultancy.com",
   company: "Elephas Consultancy AB",
-  address: { street: "Edavägen 9", city: "Kosta", country: "Sweden", countrySv: "Sverige" },
+  orgnr: "559337-5636",
+  founded: "2021",
+  address: { street: "Edavägen 9", postcode: "365 43", city: "Kosta", country: "Sweden", countrySv: "Sverige" },
   linkedin: "https://www.linkedin.com/in/leonardhulley",
   formspree: "https://formspree.io/f/maqlkbjw"
 };
